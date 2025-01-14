1d1813a8854ffd14d7fe8f5fa61efa3b5d09e109
Cyber UB <cybertguserbot@gmail.com>
2025-01-14 23:36:55 +0530

    Update notes.py

--- modules/notes.py
+++ modules/notes.py
@@ -15,7 +15,7 @@ async def save_note(client: Client, message: Message):
     except (errors.RPCError, ValueError, KeyError):
         # group is not accessible or isn't created
         chat = await client.create_supergroup(
-            "Moon_Userbot_Notes_Filters", "Don't touch this group, please"
+            "Cyber_Userbot_Notes_Filters", "Don't touch this group, please"
         )
         db.set("core.notes", "chat_id", chat.id)
 
